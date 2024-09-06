fc20972b71ce0ae9a7ecbfbf7dd361a9155df86f
mKangur <kangur.marcus@gmail.com>
2024-09-06 11:38:04 +0300

    test game loop

---
+++ frontend/src/components/arena.jsx
@@ -0,0 +1,14 @@
+import { LAR } from "../framework"
+
+const Arena = () => {
+
+    const deltaTime = 0;
+
+    return (
+        <div>
+            <h1>DeltaTime: {deltaTime}</h1>
+        </div>
+    )
+}
+
+export default Arena;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,39 @@ import { LAR } from './framework';
 import '../public/styles.css';
 import App from './app';
 import Lobby from './components/lobby';
+import Arena from './components/arena';
 
 // let appElement = <App />
 // LAR.render(appElement, document.body);
 
 LAR.render(<Lobby />, document.body)
+
+
+
+// game loop
+// https://www.codeease.net/programming/javascript/delta-time-js - solution 2
+let lastFrameTime = 0;
+function gameLoop(currentTime) {
+    // Calculate delta time
+    const deltaTime = (currentTime - lastFrameTime) / 1000; // Convert to seconds
+    lastFrameTime = currentTime;
+    
+    // Update game logic based on delta time
+    updateGameLogic(deltaTime);
+    renderGame();
+
+    // Request the next frame
+    requestAnimationFrame(gameLoop);
+}
+function updateGameLogic(deltaTime) {
+    // Update game objects based on delta time
+    // e.g., move objects, change their properties, etc.
+    //   player.x += player.speed * deltaTime;
+}
+function renderGame() {
+    console.log("running")
+    LAR.render(<Arena />, document.body)
+
+}
+// Start the game loop
+requestAnimationFrame(gameLoop);

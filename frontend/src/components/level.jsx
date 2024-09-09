e6bcb79af9425d0c0dfee583f25052acd27d4b43
mKangur <kangur.marcus@gmail.com>
2024-09-09 15:20:03 +0300

    working on level

--- frontend/src/components/level.jsx
+++ frontend/src/components/level.jsx
@@ -1,5 +1,10 @@
 import { LAR } from "../framework";
 
+    // cell class ( X Y blocktype onfire, hasbomb )
+
+    // initialize level (box class, loopib need labi, genereerib divid)
+
+
 const Level = () => {
   let players = [
     { name: "Neo", eliminations: 0 },
@@ -7,22 +12,48 @@ const Level = () => {
     { name: "Mr. Reagan", eliminations: 2 },
     { name: "Trinity", eliminations: 0 },
   ];
+  class Cell {
+    constructor(x, y, blockType, onFire, hasBomb, element) {
+      this.x = x;
+      this.y = y;
+      this.blockType = blockType;
+      this.onFire = onFire;
+      this.hasBomb = hasBomb;
+      this.element = element;
+    }
+  }
+
+  const GRID_LENGTH = 11;
+  const GRID_WIDTH = 13;
+
+  const initializeGrid = () => {
+    let cells = [];
+    for (let row = 0; row < GRID_WIDTH; row++) {
+      for (let column = 0; column < GRID_LENGTH; column++) {
+        cells.push(
+          // TODO: initialize cell with data from websokk
+          // new Cell(i, j, 0, false, false)
+        );
+      }
+    }
+    return cells;
+  };
 
   const OuterWalls = () => {
-    let numBoxes = 20;
+    let numBoxes = 11;
 
     const hardBox = (numBoxes) => {
       const boxes = [];
       for (let i = 0; i < numBoxes; i++) {
-        boxes.push(<div key={i} className="indestructible-box"></div>);
+        boxes.push(<div key={i} className="box-1"></div>);
       }
       return boxes;
     };
 
     return (
       <div>
-        <div class="topBoxes">{hardBox(numBoxes+4)}</div>
-        <div class="bottomBoxes" style={"bottom: 0"}>{hardBox(numBoxes+4)}</div>
+        <div class="topBoxes">{hardBox(numBoxes + 2)}</div>
+        <div class="bottomBoxes" style={"bottom: 0"}>{hardBox(numBoxes + 2)}</div>
         <div class="leftBoxes">{hardBox(numBoxes)}</div>
         <div class="rightBoxes" style={"right:0"}>{hardBox(numBoxes)}</div>
       </div>
@@ -33,13 +64,15 @@ const Level = () => {
   const GameArea = () => {
 
     return (
-    <div className="inGame">
-        <div className="empty-box"></div>
-        <div className="destructible-box"></div>
-        <div className="destructible-box"></div>
-        <div className="destructible-box"></div>
-    </div>
-  )}
+      <div className="inGame">
+        <div className="box-0"></div>
+        <div className="box-2"></div>
+        <div className="box-2"></div>
+        <div className="box-1"></div>
+        <div className="box-2"></div>
+      </div>
+    )
+  }
 
   return (
     <div id="level">
@@ -54,7 +87,7 @@ const Level = () => {
       </div>
       <div className="gameArea" id="gameArea">
         <OuterWalls />
-        <GameArea/>
+        <GameArea />
       </div>
     </div>
   );

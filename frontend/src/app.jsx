bee015deb5333237ceffaf260f832feaff373422
lhaavel <lukas.haavel@gmail.com>
2024-09-14 15:36:34 +0300

    initial

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -3,15 +3,21 @@ import Lobby from './components/lobby';
 
 const App = () => {
 
-
+    const [isRegistered, registerPlayer] = LAR.useState(false)
+    const [players, updatePlayers] = LAR.useState([])
+    //startWS
+    //registerPlayer
 
 
     return (
         <body>
+            {isRegistered ? 
+            <div id="game"></div> : 
             <div id="lobby">
-                <Lobby />
-            </div>
-            <div id="game"></div>
+                <Lobby registerPlayer={registerPlayer} players={players} updatePlayers={updatePlayers} />
+            </div>}
+            
+            
         </body>
     )
 };

31cf431d12e9e65a01b2f1ecb3bd4a73c4d5295c
antierisalu <antikaz@gmail.com>
2024-09-22 20:37:14 +0300

    rm color check and unused imports, ws.onmessage=null on death

--- frontend/src/components/players.jsx
+++ frontend/src/components/players.jsx
@@ -81,7 +81,6 @@ const Players = (prop) => {
               if (data.player.username == client.name){
                 players = []
                 prop.killPlayer(false)
-                ws.onmessage = null
               } else {
                 players.forEach((player)=>{
                   if (player.name === data.player.username){
@@ -89,6 +88,7 @@ const Players = (prop) => {
                   }
                 })
               }
+              ws.onmessage = null
               break;
             default:
               console.log("unknown data:",data)

6f2083fff11b1f93214f0e742340ce2054742b19
rainpraks <133800439+rainpraks@users.noreply.github.com>
2024-09-23 13:07:25 +0300

    update UI

--- frontend/src/components/chat.jsx
+++ frontend/src/components/chat.jsx
@@ -16,6 +16,62 @@ const Chat = (prop) => {
     };
 
     return (
+        <div className= "chat-container">
+            <div className="chat-messages">
+                {prop.messages.map((msg, index) => (
+                    <div key={index} className="messages">
+                        {msg.content}
+                    </div>
+                ))}
+            </div>
+
+            <div className="input-container">
+                <textarea  
+                    value={currentMessage}
+                    onInput={(e) =>  {
+                        setCurrentMessage(e.target.value)}
+                    }   
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            sendChatMessage(e)
+                        }
+                    }}
+                    placeholder="Type a message..."
+                    rows="2"
+                    className="chat-textarea"
+                />
+                <button 
+                    onClick={sendChatMessage}
+                    type="submit"
+                    className="send-button bomb-icon"
+                >
+                </button>
+            </div>    
+        </div>
+    );
+};
+
+export default Chat;
+
+
+/* import { LAR } from "../framework";
+import { sendMessage } from '../websocket.js'
+
+const Chat = (prop) => {  
+    const [currentMessage, setCurrentMessage] = LAR.useState(""); 
+
+    // Handle sending messages
+    const sendChatMessage = (event) => {
+        const message = {
+            type: "chat_message", 
+            content: currentMessage // current message input by the user
+        }
+        sendMessage(JSON.stringify(message)); // Send message over WebSocket
+        setCurrentMessage(""); // Clear input after sending
+        
+    };
+
+    return (
         <div style="position: absolute; left: 750px; top: -9px; padding: 10px; width: 200px;">
             <div style="height: 585px; width: overflow-y: scroll; background-color: #888; padding: 10px; border: 2px solid black;"> 
                 {prop.messages.map((msg, index) => (
@@ -50,22 +106,24 @@ const Chat = (prop) => {
                     }}
                 />
                 <button 
-                onClick={sendChatMessage} 
-                type="submit" 
-                style={{
-                    padding: '8px 16px', 
-                    backgroundColor: '#888', 
-                    color: 'white', 
-                    border: 'none', 
-                    cursor: 'pointer',
-                    borderRadius: '4px'
+                    onClick={sendChatMessage}
+                    type="submit"
+                    style={{
+                        width: '30px',
+                        height: '30px',
+                        backgroundColor: 'transparent',
+                        border: 'none',
+                        cursor: 'pointer',
+                        backgroundImage: 'url("../../assets/bomb.png")', 
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center', 
+                        backgroundRepeat: 'no-repeat',
                     }}
-                >                    
-                    Send
+                >
                 </button>
             </div>    
         </div>
     );
 };
 
-export default Chat;
+export default Chat; */
